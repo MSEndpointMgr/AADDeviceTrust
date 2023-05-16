8a182d4e4ab0b7fa7cb890e968254e612458f08e
Max Allen <125095090+AzureToTheMax@users.noreply.github.com>
2023-05-16 17:40:39 -0400

    1.0.2

--- AADDeviceTrust.Client/Public/New-AADDeviceTrustBody.ps1
+++ AADDeviceTrust.Client/Public/New-AADDeviceTrustBody.ps1
@@ -13,17 +13,18 @@ function New-AADDeviceTrustBody {
         Author:      Nickolaj Andersen
         Contact:     @NickolajA
         Created:     2022-03-14
-        Updated:     2023-05-10
+        Updated:     2023-05-14
 
         Version history:
         1.0.0 - (2022-03-14) Script created
-        1.0.1 - (2023-05-10) @AzureToTheMax - Updated to no longer use Thumbprint field, now redundant.
+        1.0.1 - (2023-05-10) @AzureToTheMax - Updated to no longer use Thumbprint field, no redundant.
+        1.0.2 - (2023-05-14) @AzureToTheMax - Updating to pull the Azure AD Device ID from the certificate itself.
     #>
     [CmdletBinding(SupportsShouldProcess = $true)]
     param()
     Process {
         # Retrieve required data for building the request body
-        $AzureADDeviceID = Get-AzureADDeviceID
+        $AzureADDeviceID = Get-AzureADDeviceID # Still needed to form the signature.
         $CertificateThumbprint = Get-AzureADRegistrationCertificateThumbprint
         $Signature = New-RSACertificateSignature -Content $AzureADDeviceID -Thumbprint $CertificateThumbprint
         $PublicKeyBytesEncoded = Get-PublicKeyBytesEncodedString -Thumbprint $CertificateThumbprint
@@ -31,9 +32,9 @@ function New-AADDeviceTrustBody {
         # Construct client-side request header
         $BodyTable = [ordered]@{
             DeviceName = $env:COMPUTERNAME
-            DeviceID = $AzureADDeviceID
+            #DeviceID = $AzureADDeviceID - Will be pulled from the key.
             Signature = $Signature
-            #Thumbprint = $CertificateThumbprint
+            #Thumbprint = $CertificateThumbprint - Will be pulled from the key.
             PublicKey = $PublicKeyBytesEncoded
         }
 
